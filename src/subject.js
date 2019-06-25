5f329d6ec4e02168ae1a95f1800a67f5b564ddc6
Lucas deGomez <lucasdegomez@gmail.com>
2019-06-25 12:20:00 +0200

    Fresh start :seedling:

---
+++ src/subject.js
@@ -0,0 +1,5 @@
+export default class MyClass {
+  someMethod() {
+    return false;
+  }
+}
---
+++ tests/test.js
@@ -0,0 +1,8 @@
+import MyClass from '../src/subject';
+
+describe('When ', () => {
+  const myClass = new MyClass();
+  it('should behave...', () => {
+    expect(myClass.someMethod()).toBeFalsy();
+  });
+});
